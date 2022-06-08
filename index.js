dbb5a2e02fc4df968c031c88309ce9ec2bdc35b0
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-06-08 17:15:32 +0600

    create delete api for delete information

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express()
 
@@ -8,7 +9,7 @@ app.use(cors());
 app.use(express.json()) //use to get data req.body
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.e7zdr.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
@@ -17,7 +18,7 @@ async function run() {
         const personalDetailsCollection = client.db('applicationProcess').collection('personalDetails');
         const businessDetailsCollection = client.db('applicationProcess').collection('businessDetails');
         const loanDetailsCollection = client.db('applicationProcess').collection('loanDetails');
-       
+
         //get personal Details
         app.get('/get-personalDetails', async (req, res) => {
             const query = {};
@@ -32,6 +33,13 @@ async function run() {
             const result = await personalDetailsCollection.insertOne(information)
             res.send(result)
         });
+        // Delete personal Details
+        app.delete('/delete-personalDetails/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) };
+            const result = await personalDetailsCollection.deleteOne(query)
+            res.send(result)
+        })
         //get Business details
         app.get('/get-Businessdetails', async (req, res) => {
             const query = {};
@@ -39,25 +47,39 @@ async function run() {
             const Businessdetails = await cursor.toArray();
             res.send(Businessdetails);
         })
-         // add Business Details
-         app.post('/add-Businessdetails', async (req, res) => {
+        // add Business Details
+        app.post('/add-Businessdetails', async (req, res) => {
             const information = req.body
             const result = await businessDetailsCollection.insertOne(information)
             res.send(result)
         });
+        // Delete Business Details
+        app.delete('/delete-Businessdetails/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) };
+            const result = await businessDetailsCollection.deleteOne(query)
+            res.send(result)
+        })
         //get Loan Application details 
         app.get('/get-LoanDetails', async (req, res) => {
             const query = {};
-            const cursor = loanDetailsCollection .find(query);
+            const cursor = loanDetailsCollection.find(query);
             const LoanDetails = await cursor.toArray();
             res.send(LoanDetails);
         })
-         // add Loan Application details
-         app.post('/add-LoanDetails', async (req, res) => {
+        // add Loan Application details
+        app.post('/add-LoanDetails', async (req, res) => {
             const information = req.body
-            const result = await loanDetailsCollection .insertOne(information)
+            const result = await loanDetailsCollection.insertOne(information)
             res.send(result)
         });
+        // Delete Business Details
+        app.delete('/delete-LoanDetails/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) };
+            const result = await loanDetailsCollection.deleteOne(query)
+            res.send(result)
+        })
     }
     finally {
 
